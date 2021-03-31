6d6ed465faddfe15145a36372b72c10fdc5b6fc0
Y-J park <dPjin0912@gmail.com>
2021-03-31 10:25:03 +0900

    inner class with this

--- app/src/main/java/com/example/btnwiinnerclass/MainActivity.java
+++ app/src/main/java/com/example/btnwiinnerclass/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.btnwiinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,7 +19,18 @@ public class MainActivity extends AppCompatActivity {
         mbutton1 = findViewById(R.id.button1);
         mTextView = findViewById(R.id.textView1);
 
-
         mbutton1.setOnClickListener(new MyOnCilckListener(this));
+    }
+
+    private class MyOnCilckListener implements View.OnClickListener {
+        MainActivity mainActivity;
+        public MyOnCilckListener(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mainActivity.mTextView.setText("You Clickes button 1");
+        }
     }
 }
